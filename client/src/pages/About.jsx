5a6aca385a66565c1c3655f630f8acf235ab49b1
Regina <ritschi25@gmx.de>
2024-03-07 10:40:50 +0100

    first commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About 1001 Bricks Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to 1001 Bricks Blog!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
